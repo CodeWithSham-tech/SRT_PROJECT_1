8051e01e0fd10c52e649fa73ad0ef69bd55db4ef
Ehtasham Ali <codewithsham21@gmail.com>
2024-10-08 07:53:06 -0700

    First Task

---
+++ ProductLandingPage/app.js
@@ -0,0 +1,13 @@
+function changeImge(){
+    var img = document.getElementById("imgeCh");
+    imgeCh.src = "images/p2-image.jpg";
+
+}
+function oldimg(){
+    var img = document.getElementById("imgeCh");
+    imgeCh.src = "images/p-image.jpg";
+}
+
+
+    
+
